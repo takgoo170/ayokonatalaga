73465fd9f677d21e259ce334a771385231283a4d
takgoo170 <eddiejrsapian@gmail.com>
2025-08-21 10:42:29 +0800

    Update nakakatuwatalaga.lua

--- nakakatuwatalaga.lua
+++ nakakatuwatalaga.lua
@@ -2,8 +2,8 @@
 -- wala eh.
 -- ganon talaga.
 
-local redz_ui = loadstring(game:HttpGet("https://raw.githubusercontent.com/takgoo170/ui_source/refs/heads/main/redzhub.ui.luau"))()
-local Window = redz_ui:MakeWindow({
+local redzlib = loadstring(game:HttpGet("https://raw.githubusercontent.com/takgoo170/ui_source/refs/heads/main/redzhub.ui.luau"))()
+local Window = redzlib:MakeWindow({
     Title = "Kai Hub | Meme Sea",
     Subtitle = "by Kai Scripts",
     SaveFolder = "save_ui"
@@ -14,7 +14,7 @@ Window:AddMinimizeButton({
     Corner = { CornerRadius = UDim.new(35, 1) },
   })
 
-redz_ui:SetTheme("Purple")
+redzlib:SetTheme("Purple")
 
 local Tab1 = Window:MakeTab({ "Example", "house" })
 local Tab2 = Window:MakeTab({ "Example Ui", "carrot" })
